9b25ba058e333f5825d23e1e4aaec5377f48483b
The-Flying-Dev <leandranm@outlook.com>
2022-08-05 12:50:17 -0700

    Controller methods

--- app/controllers/api/v1/meetups_controller.rb
+++ app/controllers/api/v1/meetups_controller.rb
@@ -1,13 +1,41 @@
 class Api::V1::MeetupsController < ApplicationController
   def index
+    meetup = Meetup.all.order(created_at: :desc)
+    render json: meetup
   end
 
   def create
+    meetup = Meetup.create!(meetup_params)
+    if meetup 
+      render json: meetup
+    else   
+      render json: meetup.errors
+    end
   end
 
   def show
+    if meetup 
+      render json: meetup
+    else   
+      render json: meetup.errors
+    end
   end
 
+  # &. -> Safe Navigation Operator, checks the value exists before calling the destroy method
   def destroy
+    meetup&.destroy
+    render json: { message: 'Meetup was successfully deleted' }
   end
+
+  private 
+
+  def meetup_params 
+    params.permit(:meetup).require(:title, :address, :description, :image)
+  end
+
+  # ||= will not return undefined if the object does not exist
+  def meetup 
+    @meetup ||= Meetup.find(params[:id])
+  end
+
 end
